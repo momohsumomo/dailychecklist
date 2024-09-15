079e1c100f1a2b0e1b6573be89747cc31d3dba14
momohsu <wmhsu82@gmail.com>
2024-09-16 00:52:55 +0800

    移除敏感信息

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// 載入環境變數
+require('dotenv').config();
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+// 其他代碼...
+
+
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
